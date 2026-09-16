13888950731438ac3d6ccc03f6431c9f26a896d4
soldiv86-rgb <soldiv86@gmail.com>
2026-09-16 13:07:29 +0800

    Update loader.lua

--- core/loader.lua
+++ core/loader.lua
@@ -6,7 +6,9 @@ local BASE_URL = "https://raw.githubusercontent.com/soldiv86-rgb/DivineSouls/mai
 -- Map Roblox game IDs to the folder name inside /games
 local GAME_MAP = {
 	["10035204815"] = "RideAPet", 
-	["10742439589"] = "Campfire",-- replace with the real GameId of the game this maps to
+	["10742439589"] = "Campfire",
+	["10735530810"] = "EggUpgradeTree",
+	-- replace with the real GameId of the game this maps to
 	-- ["1111111"] = "game2",
 }
 
